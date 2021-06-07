772d3eb637401531e89a76311c28fe6cd2a1afef
Mario Gutiérrez <marioagtzc@hotmail.com>
2021-06-07 15:58:07 -0500

    ADN update

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    output: {
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    }
+                ]
+            },
+            {
+                test: /styles.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif|pdf|ico)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    optimization: {},
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Mi Webpack App',
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/privacy/index.html',
+            filename: './privacy/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+}
---
+++ webpack.prod.js
@@ -0,0 +1,89 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    output: {
+        clean: true,
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    }
+                ]
+            },
+            {
+                test: /styles.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    }
+                ],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif|pdf|ico)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/privacy/index.html',
+            filename: './privacy/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+}
